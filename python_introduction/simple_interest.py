be2e1921cc8ec81af6affcdb5f1f50f3de70ed86
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-20 16:54:06 +0300

    Created a simple interest calculator app

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculating interest using the formula I = P * R * T
+interest = principal * rate * time
+
+#Printing the results i.e interest
+print("The simple interest is: ", interest)
