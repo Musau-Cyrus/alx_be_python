6c53d90160e69cd8f5f49d7acd5c636af8d20445
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-20 16:44:10 +0300

    Performed basic operations on two numbers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition of number1 and number2
+sum = number1 + number2
+
+#Subracting te two numbers
+difference = number1 -number2
+
+# Product of the two numbers
+product = number1 * number2
+
+#Printing the outputs of the sum, difference and products of number1 and number2
+print("Addition of", number1, "and", number2, "is", sum)
