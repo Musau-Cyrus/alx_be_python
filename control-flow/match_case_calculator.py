aea24814bb025fc6a376378a251f803f917a0719
Cyrus <162912994+Musau-Cyrus@users.noreply.github.com>
2025-05-28 12:20:35 +0000

    Created a calculator using match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #Operand prompt
-operand = input(print("Choose the operation (+, -, *, /): "))
+operand = input("Choose the operation (+, -, *, /): ")
 
 match operand:
     case "+":
