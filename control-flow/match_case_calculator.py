a89d615d510e00acc3173ed33ffa0e638a463ee4
Cyrus <162912994+Musau-Cyrus@users.noreply.github.com>
2025-05-28 12:25:24 +0000

    Created a calculator using match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,11 @@
+#Prompt the user to enter num1 and num2
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #Operand prompt
 operation = input("Choose the operation (+, -, *, /): ")
 
+# A match case control flow that controls the type of input that a user enters
 match operation:
     case "+":
         result = num1 + num2
