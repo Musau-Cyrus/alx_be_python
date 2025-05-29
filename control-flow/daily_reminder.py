8d73027e11069258f98c6ee1140216418d92a80a
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-29 10:33:52 +0300

    Created a daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,29 @@
 # Prompt the user for a ingle task
-task = str(input("Enter your task: "))
+task = str(input("Enter your task:"))
 priority = str(input("Priority (high/medium/low): ")).lower()
-time = input("Is it time bound? ").lower()
+time = input("Is it time-bound? (yes/no): ").lower()
 
 # A match case to raect differently
 match priority:
     case "high": 
         if time == "yes":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         elif time == "no":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
             print("Invalid time priority!")
     case "medium":
         if time == "yes":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         elif time == "no":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
             print("Invalid time priority!")
     case "low":
         if time == "yes":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         elif time == "no":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate attention today!")
+            print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
             print("Invalid time priority!")
     case _:
