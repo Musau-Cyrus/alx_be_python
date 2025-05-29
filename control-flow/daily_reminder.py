11af2e852f9f7da0272fc171c1afff070f34b9aa
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-05-29 10:41:22 +0300

    Created a daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,30 +1,30 @@
 # Prompt the user for a ingle task
 task = str(input("Enter your task:"))
 priority = str(input("Priority (high/medium/low): ")).lower()
-time = input("Is it time-bound? (yes/no): ").lower()
+time_bound = input("Is it time_bound-bound? (yes/no): ").lower()
 
 # A match case to raect differently
 match priority:
     case "high": 
-        if time == "yes":
+        if time_bound == "yes":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
-        elif time == "no":
+        elif time_bound == "no":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
-            print("Invalid time priority!")
+            print("Invalid time_bound priority!")
     case "medium":
-        if time == "yes":
+        if time_bound == "yes":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
-        elif time == "no":
+        elif time_bound == "no":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
-            print("Invalid time priority!")
+            print("Invalid time_bound priority!")
     case "low":
-        if time == "yes":
+        if time_bound == "yes":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
-        elif time == "no":
+        elif time_bound == "no":
             print(f"Reminder:", '{task}', "is a", priority, "priority task that requires immediate attention today!")
         else:
-            print("Invalid time priority!")
+            print("Invalid time_bound priority!")
     case _:
         print("Priority entered!")
